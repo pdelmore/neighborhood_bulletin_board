226964ec5a27a6c87110cf6b2e345d4c0b7533ce
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-28 21:41:40 +0000

    some styling and adding static content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,9 @@ def index
   render({ :template => "/post_templates/index.html.erb" })
 end
 
+def show
+
+render({ :template => "/post_templates/show.html.erb" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-
+get("/", { :controller => "posts", :action=> "index" })
 get("/posts", { :controller => "posts", :action=> "index" })
+
+get("/posts/details", { :controller => "posts", :action=> "show" })
 
 
 end

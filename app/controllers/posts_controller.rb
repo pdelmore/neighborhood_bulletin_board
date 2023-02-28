31d035676fbd12803c34657264ef7024763186c6
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-28 22:30:34 +0000

    list of posts on main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,16 @@ class PostsController < ApplicationController
 
 def index
 
+  matching_posts = Post.all
+
+  @posts = matching_posts.order(:created_at)
+
   render({ :template => "/post_templates/index.html.erb" })
 end
 
 def show
+
+  all_comments = Comment.all
 
 render({ :template => "/post_templates/show.html.erb" })
 end

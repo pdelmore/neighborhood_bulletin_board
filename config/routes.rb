b6fae45ffa333e61d7d9b1d72203989763aa438e
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-01 18:37:35 +0000

    added validations

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ get("/posts/details", { :controller => "posts", :action=> "show" })
 
 get("/add_post", { :controller => "posts", :action => "create_post"})
 
-
 get("/insert_comment", { :controller => "posts", :action => "add_comment" })
 
 get("/post/details/:post_id", { :controller => "posts", :action => "show" })
+
 get("/delete_post/:post_id", { :controller => "posts", :action => "delete_post"})
 
 get("/post/edit/:post_id", { :controller => "posts", :action => "edit_post"})

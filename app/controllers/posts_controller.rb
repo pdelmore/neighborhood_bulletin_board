cee00c7a493fb920bb1943a3e95f2e3a105e93d5
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-01 15:37:07 +0000

    create a new post is working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,21 @@ def show
 render({ :template => "/post_templates/show.html.erb" })
 end
 
+def create_post
+
+#     Parameters: {"query_post_title"=>"help needed asap!", "query_post_body"=>"Post body"}
+
+post_title = params.fetch("query_post_title")
+post_body = params.fetch("query_post_body")
+
+new_post = Post.new
+
+new_post.title = post_title
+new_post.body = post_body
+
+new_post.save
+
+render({ :template => "/post_templates/submitted.html.erb" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ get("/posts", { :controller => "posts", :action=> "index" })
 
 get("/posts/details", { :controller => "posts", :action=> "show" })
 
+get("/add_post", { :controller => "posts", :action => "create_post"})
+
 
 end
